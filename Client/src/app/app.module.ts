3fd32f8cd11946d55317a35cc274674b3fed04a0
mmraihan <mm_raihan@live.com>
2022-10-19 15:11:41 +0600

    Design Grid and list Component.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { SuggestedProductsComponent } from './suggested-products/suggested-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    SuggestedProductsComponent
   ],
   imports: [
     BrowserModule
