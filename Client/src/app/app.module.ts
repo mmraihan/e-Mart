42c19cfccdca6f245c0f6ac8dac2b23c9e702c44
mmraihan <mm_raihan@live.com>
2022-10-19 15:51:29 +0600

    Add home component.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { SuggestedProductsComponent } from './suggested-products/suggested-products.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    SuggestedProductsComponent
+    SuggestedProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
