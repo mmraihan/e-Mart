64e17d15939755b3eb968af03699f6704bdb0edd
mmraihan <mm_raihan@live.com>
2022-10-20 01:23:13 +0600

    Add buttons (grid,list,sort by) using ngClass.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { SuggestedProductsComponent } from './suggested-products/suggested-products.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     SuggestedProductsComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
